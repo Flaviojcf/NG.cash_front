6b39de3c70133dfbee5e033236b7b15d79e3199a
Flavio <fjcf@poli.br>
2022-11-15 19:42:18 -0300

    feat: add some colors to default Theme.

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,3 +1,11 @@
 export const defaultTheme = {
-  colors: {},
+  colors: {
+    black: "#000000",
+    "purple-100": "#7D2CFF",
+    "purple-200": "#8850FF",
+    green: "#01CA30",
+    white: "#FFFFFF",
+    blue: "#004787",
+    gray: "#C2C2C2",
+  },
 } as const;
